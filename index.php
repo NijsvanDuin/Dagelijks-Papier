0a8381728dc4b9c7f1cc83cc14563c07343f1a2b
333736 <90184972+NMbo@users.noreply.github.com>
2022-02-16 16:56:03 +0100

    Home Pagina toegevoegt, grid css toegevoegt(bedankt Jesse!) navbar toegevoegt(alweer bedankt Jesse!). De content komt op een bepaalde plek van de pagina te staan(Hij werkt al maar zit nog achter de navbar)

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-testtt
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/grid.css">
+    <title>Daagelijks Papier</title>
+</head>
+
+<body>
+    <main>
+        <section class="container">
+            <div class="row">
+                <div class="col-12"><?php include("./navbar.php"); ?></div>
+            </div>
+        </section>
+        <section class="container">
+            <div class="row">
+                <div class="col-12">
+                    <?php include("./content.php"); ?>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
