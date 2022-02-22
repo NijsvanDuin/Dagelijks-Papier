ca07ba908cbc2fab28cec3184da1472e4229cbc4
LAPTOP-VAN-BART\bartr <333895@student.mboutrecht.nl>
2022-02-22 15:39:26 +0100

    voorbeeld nieuwtjes gedaan voor overzicht pagina en home pagina

--- home.php
+++ home.php
@@ -1,1 +1,44 @@
-Home
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/grid.css">
+    <title>Daagelijks Papier</title>
+</head>
+
+<body>
+
+    <div class="col-l-12">
+        <div class="card col-5">
+            <img src="./img/unknown.png" alt="Avatar" style="width:100%">
+            <div class="container">
+                <h3><b>Johnson: 'Geen twijfel dat Rusland invasie uitvoert'</b></h3>
+                <p>Er is geen enkele twijfel dat Rusland het grondgebied van Oekraïne schendt, zegt de Britse premier Boris Johnson in een toespraak in het Britse parlement. "Er is nog geen sprake van een volledige invasie, maar de ondernomen acties maken duidelijk dat Poetin nog verder wil gaan om Oekraïne te onderdrukken."
+
+                    Volgens Johnson heeft Poetin "alle kansen gekregen om een diplomatieke oplossing te accepteren", maar moeten de diplomatieke pogingen van het westen als tevergeefs beschouwd worden als de situatie niet verandert.
+
+                    De Britten reageren daarom met zware sancties voor Russische banken, organisaties en individuen. Zo worden banktegoeden bevroren, mogen Britse bedrijven geen zaken meer doen met enkele Russische bedrijven en krijgen enkele hooggeplaatste Russen een reisverbod. Volgens Johnson betreft nog maar "een eerste lading" aan sancties.</p>
+            </div>
+        </div>
+        <div class="card col-5">
+            <img src="./img/nieuwtje.png" alt="Avatar" style="width:100%">
+            <div class="container">
+                <h3><b>Bij een steekpartij op het Rotterdam Designcollege is dinsdagmiddag een leerling gewond geraakt, meldt de politie.</b></h3>
+                <p>Bij een steekpartij op het Rotterdam Designcollege is dinsdagmiddag een leerling gewond geraakt, meldt de politie.
+
+                    Een woordvoerder laat weten dat het slachtoffer meerdere steekwonden heeft opgelopen, maar aanspreekbaar de ambulance in is gegaan. Daarna is de jongen, die "waarschijnlijk een leerling van de school is en dus een tiener", naar het ziekenhuis gebracht.
+
+                    De politie heeft nog niemand aangehouden, maar "is hard op zoek naar de dader of daders". De politie doet onderzoek op de school aan de Beukelsdijk. "We vragen andere leerlingen of ze iets hebben gezien of meegemaakt en stellen camerabeelden van binnen en buiten de school veilig", aldus de politiewoordvoerder.
+
+                    Het Designcollege is dezelfde school waar de zestienjarige Humeyra eind 2018 werd doodgeschoten door haar inmiddels veroordeelde ex-vriend Bekir E.</p>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
--- overzicht.php
+++ overzicht.php
@@ -1,1 +1,48 @@
-Overzicht
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/grid.css">
+    <title>Daagelijks Papier</title>
+</head>
+
+<body>
+
+    <div class="col-l-12">
+        <div class="card col-3">
+            <img src="./img/unknown.png" alt="Avatar" style="width:100%">
+            <div class="container">
+                <h3><b>Johnson: 'Geen twijfel dat Rusland invasie uitvoert'</b></h3>
+                <p>Er is geen enkele twijfel dat Rusland het grondgebied van Oekraïne schendt, zegt de Britse premier Boris Johnson in een toespraak in het Britse parlement.
+
+                </p>
+            </div>
+        </div>
+        <div class="card col-3">
+            <img src="./img/nieuwtje.png" alt="Avatar" style="width:100%">
+            <div class="container">
+                <h3><b>Bij een steekpartij op het Rotterdam Designcollege is dinsdagmiddag een leerling gewond geraakt, meldt de politie.</b></h3>
+                <p>Bij een steekpartij op het Rotterdam Designcollege is dinsdagmiddag een leerling gewond geraakt, meldt de politie.
+
+
+
+                </p>
+            </div>
+        </div>
+        <div class="card col-3">
+            <img src="./img/nieuwtje.png" alt="Avatar" style="width:100%">
+            <div class="container">
+                <h3><b>Bij een steekpartij op het Rotterdam Designcollege is dinsdagmiddag een leerling gewond geraakt, meldt de politie.</b></h3>
+                <p>Bij een steekpartij op het Rotterdam Designcollege is dinsdagmiddag een leerling gewond geraakt, meldt de politie.
+
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
