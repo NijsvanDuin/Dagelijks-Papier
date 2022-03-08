9b55680e6116388efa9fa4e41ac18147768078b8
333736 <90184972+NMbo@users.noreply.github.com>
2022-03-08 16:23:57 +0100

    Kleine placehorder in het register input gemaakt

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
             <form action="./index.php?content=register_script" method="post">
                 <div class="form-group">
                     <label for="InputEmail">Vul hier uw email in:</label> <br>
-                    <input name="email" type="email" class="form-control informReg" id="InputEmail" aria-describedby="emailHelp">
+                    <input name="email" type="email" class="form-control informReg" id="InputEmail" aria-describedby="emailHelp" placeholder="E-mail">
                     <br>
                 </div>
                 <button type="submit" class="btn-register">Registeer</button>
