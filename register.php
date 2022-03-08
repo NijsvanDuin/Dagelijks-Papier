c7e5433fc8b4882278d319b235c95543ecc2f2ad
333736 <90184972+NMbo@users.noreply.github.com>
2022-03-08 16:22:35 +0100

    Styling aan de masseges en register button gegeven

--- register.php
+++ register.php
@@ -3,11 +3,11 @@
         <div class="col-12 col-sm-6">
             <form action="./index.php?content=register_script" method="post">
                 <div class="form-group">
-                    <label for="InputEmail">Vul hier uw email in:</label>
-                    <input name="email" type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp">
+                    <label for="InputEmail">Vul hier uw email in:</label> <br>
+                    <input name="email" type="email" class="form-control informReg" id="InputEmail" aria-describedby="emailHelp">
                     <br>
                 </div>
-                <button type="submit" class="btn btn-secondary btn-lg">Registeer</button>
+                <button type="submit" class="btn-register">Registeer</button>
             </form>
         </div>
     </div>
