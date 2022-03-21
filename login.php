0e16e7e595cb43cf6de2121d43c2767c88f4dda3
333736 <90184972+NMbo@users.noreply.github.com>
2022-03-21 09:07:53 +0100

    Opmaak van de login pagina gemaakt

--- login.php
+++ login.php
@@ -1,1 +1,20 @@
-login
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <form action="./index.php?content=login_script" method="post">
+                <div class="form-group">
+                    <label for="InputEmail">Vul hier uw email in:</label> <br>
+                    <input name="email" type="email" class="form-control informReg" id="InputEmail" aria-describedby="emailHelp" placeholder="E-mail">
+                    <br>
+                    <br>
+                </div>
+                <div class="form-group">
+                    <label for="InputPassword">Vul hier uw wachtwoord in:</label> <br>
+                    <input name="password" type="password" class="form-control informReg" id="InputPassword" aria-describedby="passwordHelp" placeholder="Wachtwoord">
+                    <br>
+                </div>
+                <button type="submit" class="btn-register">Log In</button>
+            </form>
+        </div>
+    </div>
+</div>
