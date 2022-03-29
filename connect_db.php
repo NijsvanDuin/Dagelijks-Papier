9d620beb908b32a2e76c415015888ce8b12142dc
333736 <90184972+NMbo@users.noreply.github.com>
2022-03-29 15:24:20 +0200

    Connect db ALLEEN VOOR NIJS

--- connect_db.php
+++ connect_db.php
@@ -1,6 +1,6 @@
 <?php
     define("SERVERNAME", "localhost");
-    define("USERNAME", "inlog_registatie");
+    define("USERNAME", "dagelijkspapier");
     define("PASSWORD", "YWD45f4Pe7kpKwUl");
     define("DBNAME", "dagelijksPapier");
 
